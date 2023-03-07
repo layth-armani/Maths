af574ed976a99d480a30d22b53f783c3ecbefd5f
layth-armani <84923837+layth-armani@users.noreply.github.com>
2023-03-07 23:11:46 +0100

    Update code binaire.py

--- Maths expertes/code binaire.py
+++ Maths expertes/code binaire.py
@@ -2,20 +2,20 @@ import random
 
 liste = [0,1]
 
-caca= random.choices(liste, k=8)
+ca= random.choices(liste, k=8)
 
 e=0
 
-for i in range (len(caca)):
+for i in range (len(ca)):
     
-    if caca[i]==1:
+    if ca[i]==1:
         e+=1
 
 
 if e%2==0:
-    print("1",caca)
+    print("1",ca)
 else:
-    print("0",caca)
+    print("0",ca)
 
   
 
